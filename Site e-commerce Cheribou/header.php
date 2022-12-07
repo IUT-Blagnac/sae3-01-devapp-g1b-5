86fc00444d69c35fc3d7df30bb99653d3e8389c2
Yann-cmd <95311663+Yann-cmd@users.noreply.github.com>
2022-12-07 12:17:41 +0100

    Delete header.php

--- Site e-commerce Cheribou/header.php
+++
@@ -1,6 +0,0 @@
-<div class="header">
-  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lemon" />
-            CHERIBOU
-            <img src="images/DrapeauFR.jpg" height=80 alt="image france" id="imageFR"></img>
-</div>
-
