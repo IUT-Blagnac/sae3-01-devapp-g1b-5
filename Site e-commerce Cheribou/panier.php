cf69c065c4c50effc61f5568ebba4b38219352e3
Yann-cmd <95311663+Yann-cmd@users.noreply.github.com>
2022-12-08 10:27:15 +0100

    ajout page du panier

---
+++ Site e-commerce Cheribou/panier.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+     <link rel="stylesheet" href="images/styles.css">
+     <link rel="stylesheet" href="images/Panier.css">
+	<title>Mon site en PHP!</title>
+</head>
+<body>
+	<?php include("./images/header.php"); ?>
+  <main>
+    <div class="contener">
+      <button class="boutonD" style="color: black;"><b>Se déconnecter</b></button>
+      <p><b>Mon panier</b></p> 
+      
+      <p style="font-size: 2em;">X articles</p>
+      <div class="contener2">
+        <p class="text">Informations personnelles</p>
+        <div class="cont">  
+          <button class="boutonV">Valider</button> 
+        </div>
+      </div>
+      
+      
+  
+    </div>
+  </main>
+</body>
+</html>
