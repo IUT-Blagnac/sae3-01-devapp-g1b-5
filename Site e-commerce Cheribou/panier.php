2d9a113df3fe11d95c91870ef5d746bc65611dd4
Shiyu Hu <100203369+SylvainHsy@users.noreply.github.com>
2022-12-16 10:23:10 +0100

    Update panier.php

--- Site e-commerce Cheribou/panier.php
+++ Site e-commerce Cheribou/panier.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="utf-8" />
 
-    <link rel="stylesheet" href="include\Panier.css">
+    <link rel="stylesheet" href="include/Panier.css">
     <title>Mon site en PHP!</title>
 </head>
 
 <body>
-    <?php include("include\header.php"); ?>
+    <?php include("include/header.php"); ?>
     <main>
         <div class="contener">
             <button class="boutonD" style="color: black;"><b>Se déconnecter</b></button>
