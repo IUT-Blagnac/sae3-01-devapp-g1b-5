18cac944f81455a6ed5e5bfb5450a506598410bb
Yann Guillevic <yannguillevic12@gmail.com>
2022-12-19 17:35:32 +0100

    modif python

--- Programmes python/main.py
+++ Programmes python/main.py
@@ -1,13 +1,13 @@
-import json
-import yaml
-import os
-import time
+import json, yaml, os, time, signal
+
 
 import paho.mqtt.client as mqtt
 
+
 def get_data(mqtt, obj, msg):
-    global param
-    jsonMsg = json.loads(msg.payload)
+    global donneeCo2, donneeHum, donneeTemp
+    #jsonMsg = json.loads(msg.payload)
+    jsonMsg = json.loads(msg.payload.decode('utf-8'))
     donneeCo2 = jsonMsg["object"]["co2"]
     print("Concentration CO2 : ", donneeCo2, " ppm")
     donneeTemp = jsonMsg["object"]["temperature"]
@@ -19,11 +19,14 @@ def get_data(mqtt, obj, msg):
         print("Attention, le taux de CO2 dépasse les normes !")
     if donneeTemp > param["seuilMax"]["Temp"]:
         print("Attention, la température dépasse les normes !")
-    if donneeHum> param["seuilMax"]["Hum"]:
+    if donneeHum > param["seuilMax"]["Hum"]:
         print("Attention, le taux d'humidité dépasse les normes !")
 
+    signal.alarm(param["frequence"]["valeur"])
 
-    time.sleep(param["frequence"]["valeur"])
+
+def test(signum, code):
+    print("Ecriture des données...")
     ouv = os.open("fichier.txt",os.O_WRONLY|os.O_CREAT|os.O_TRUNC,0o644)
     os.write(ouv,bytes(str(donneeCo2), 'utf-8'))
     os.write(ouv,b"-")
@@ -44,6 +47,7 @@ client.connect(param["config"]["mqttserver"], param["config"]["mqttport"], 600)
 #client.subscribe([("application/1/device/24e124128c011586/event/up",0),("application/1/device/24e124128c017943/event/up",2)])
 client.subscribe(param["config"]["device"])
 
+signal.signal(signal.SIGALRM, test)
 client.on_message = get_data
 
 
