0b44ae8fe6d242e6c2a853aeae90276a794795c0
Yann-cmd <95311663+Yann-cmd@users.noreply.github.com>
2023-01-16 16:32:34 +0100

    Update ConfigController.java

--- Programmes JAVA/SAE_JavaFX/src/main/java/sae/sae_javafx/ConfigController.java
+++ Programmes JAVA/SAE_JavaFX/src/main/java/sae/sae_javafx/ConfigController.java
@@ -1,12 +1,12 @@
 package sae.sae_javafx;
 
 import config.Config;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.*;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
@@ -37,6 +37,15 @@ public class ConfigController implements Initializable {
     private Spinner<Integer> spinnerFrequence;
 
     @FXML
+    private TextField nomDeServ;
+
+    @FXML
+    private TextField numDePort;
+
+    @FXML
+    private ChoiceBox<String> device;
+
+    @FXML
     private Button buttonValiderSeuil;
 
     @FXML
@@ -54,41 +63,67 @@ public class ConfigController implements Initializable {
             // Récupérer les données dans le Map et les affecter aux spinners
 
             System.out.println(data);
+            System.out.println(data.get("config").get("mqttserver"));
             System.out.println(data.get("frequence").get("valeur"));
             System.out.println(data.get("seuilMax").get("CO2"));
+
+            //recup donnée config serv
+            String nomServ = (String) data.get("config").get("mqttserver");
+            String numPort = (String) data.get("config").get("mqttport").toString();
+            String numDevice = (String) data.get("config").get("device");
+
+            //recup frequence
             int intfrequence = (int) data.get("frequence").get("valeur");
+
+            //recup seuil max des données
             int intseuilCO2 = (int) data.get("seuilMax").get("CO2");
             int intseuilTemp = (int) data.get("seuilMax").get("Temp");
             int intseuilHum = (int) data.get("seuilMax").get("Hum");
 
+            this.nomDeServ.setText(nomServ);
+            this.numDePort.setText(numPort);
+            this.device.getItems().clear();
+            this.device.getItems().addAll("Device1","Device2","tous");
+
 
             SpinnerValueFactory<Integer> valueFactoryFrequence = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 1000, intfrequence);
-            spinnerFrequence.setValueFactory(valueFactoryFrequence);
+            this.spinnerFrequence.setValueFactory(valueFactoryFrequence);
 
             SpinnerValueFactory<Integer> valueFactoryCO2 = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000,intseuilCO2);
-            spinnerSeuilC02.setValueFactory(valueFactoryCO2);
+            this.spinnerSeuilC02.setValueFactory(valueFactoryCO2);
 
             SpinnerValueFactory<Integer> valueFactoryTemp = new SpinnerValueFactory.IntegerSpinnerValueFactory(-100, 100,intseuilTemp);
-            spinnerSeuilTemp.setValueFactory(valueFactoryTemp);
+            this.spinnerSeuilTemp.setValueFactory(valueFactoryTemp);
 
             SpinnerValueFactory<Integer> valueFactoryHum = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100,intseuilHum);
-            spinnerSeuilHum.setValueFactory(valueFactoryHum);
+            this.spinnerSeuilHum.setValueFactory(valueFactoryHum);
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
         }
+    }
 
-
+    public String returnValue(){
+        String fin = "";
+        String saisie = device.getValue();
+        if(saisie.equals("Device1")){
+            fin = "24e124128c011586";
+        } else if (saisie.equals("Device2")) {
+            fin = "24e124128c017943";
+        } else{
+            fin = "+";
+        }
+        return fin;
     }
 
     public void actionValider(ActionEvent event) throws FileNotFoundException {
 
-        Map<String, Object> dataMapConfig = new HashMap<>();
-        dataMapConfig.put("mqttserver", "chirpstack.iut-blagnac.fr");
-        dataMapConfig.put("mqttport", 1883);
-        dataMapConfig.put("device", "application/1/device/+/event/up");
+        Map<String, String> dataMapConfig = new HashMap<>();
+        dataMapConfig.put("mqttserver", this.nomDeServ.getText());
+        dataMapConfig.put("mqttport", this.numDePort.getText());
+        String valeur = returnValue();
+        dataMapConfig.put("device", "application/1/device/"+valeur+"/event/up");
 
         Map<String, Integer> dataMapFrequence = new HashMap<>();
         dataMapFrequence.put("valeur", this.spinnerFrequence.getValue());
